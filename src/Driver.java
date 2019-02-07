d0c978d9547abe89ff9cd5b7b331fcbcbf0c4298
Jake Baker <jjbaker@parktudor.org>
2019-02-07 15:08:02 -0500

    add intro text

--- src/Driver.java
+++ src/Driver.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
+
 public class Driver {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Bye");
-        System.out.println("test");
-        System.out.println("test1");
+        Scanner in = new Scanner(System.in);
+        System.out.print("1. Tic-Tac-Toe (Human vs Human)" +
+                "\n2. Tic-Tac-Toe (Human vs Basic AI)" +
+                "\n3. Tic-Tac-Toe (Human vs Pro AI)" +
+                "\n4. Connect 4 (Human vs Human)" +
+                "\n5. Connect 4 (Human vs Basic AI)" +
+                "\n6. Connect 4 (Human vs Pro AI)" +
+                "\nSelect your game (1-6): ");
+        int gameMode = in.nextInt();
     }
 }
