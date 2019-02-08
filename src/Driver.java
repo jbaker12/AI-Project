094e7ec5040666838db67efe79ca16044a90efad
Priyanka Ranga <pranga@parktudor.org>
2019-02-08 15:12:30 -0500

    Complete TTT Human vs. Human

--- src/Driver.java
+++ src/Driver.java
@@ -10,6 +10,31 @@ public class Driver {
                 "\n5. Connect 4 (Human vs Basic AI)" +
                 "\n6. Connect 4 (Human vs Pro AI)" +
                 "\nSelect your game (1-6): ");
+
         int gameMode = in.nextInt();
+
+        if (gameMode == 1) {
+            TTTBoard b = new TTTBoard();
+            int count = 0;
+            String player = "X";
+
+            while (!b.isWinner(player)) {
+                if (count%2 == 0)
+                    player = "X";
+                else
+                    player = "O";
+                count++;
+                System.out.println("");
+                System.out.print(b);
+                System.out.print("Enter you move (1-9): ");
+                int move = in.nextInt();
+                b.placePiece(Integer.toString(move), player);
+                if (b.isWinner(player)) {
+                    System.out.println("");
+                    System.out.print(b);
+                    System.out.println("PLAYER " + player + " WINS");
+                }
+            }
+        }
     }
 }
